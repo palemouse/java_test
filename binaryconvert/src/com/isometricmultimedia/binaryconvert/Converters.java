bc7563dd6eca53c363910d0adcebca05826f3e4f
palemouse <travis.gomillion@gmail.com>
2013-09-03 22:33:56 -0400

    add original source

---
+++ binaryconvert/src/com/isometricmultimedia/binaryconvert/Converters.java
@@ -0,0 +1,19 @@
+package com.isometricmultimedia.binaryconvert;
+
+public class Converters {
+	
+	public String strTo;
+	public String[] questions = new String[20];
+	
+	
+	public Converters(String strTo){
+		this.strTo = strTo;
+	}
+	
+	public static void convertTobinary(String strTo){
+		
+		
+		
+	}
+
+}
---
+++ charAt/src/charAt/CharAtComponent.java
@@ -0,0 +1,16 @@
+package charAt;
+
+public class CharAtComponent {
+
+	public static void main(String[] args) {
+		String test1 = "Hello World!";
+		String test2 = "";
+		
+		for (int k = 3; k > 0; k--) {
+			//test2 += currChar;
+			System.out.println(k);
+		}
+
+	}
+
+}
